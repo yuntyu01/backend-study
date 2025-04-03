05d53f96f306a4f74a7d8144747474c212c673ed
workingjun <kimjunhee2483@gmail.com>
2025-04-03 19:15:13 +0900

    김준희 문제 제출

--- week3/김준희/p2753.java
+++ week3/김준희/p2753.java
@@ -3,12 +3,7 @@ import java.util.Scanner;
 public class p2753 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  // 입력 객체 생성
-<<<<<<< HEAD
         int num = sc.nextInt();
-=======
-        String num_str = sc.nextLine();
-        int num = Integer.parseInt(num_str); //참조형이기 때문에 형변환은 parseInt 사용
->>>>>>> origin/Joon
 
         if (num % 4 == 0 && (num % 100 != 0 || num % 400 == 0)) {
             System.out.println("1");
