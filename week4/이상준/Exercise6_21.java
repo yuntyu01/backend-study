52125aa9c6b072d271177daa5c1b72101715a4ac
LEE SANGJUN <lsj010115@gmail.com>
2025-04-10 16:25:02 +0900

    이상준 문제 제출

---
+++ week4/이상준/Exercise6_21.java
@@ -0,0 +1,13 @@
+class Exercise6_21 {
+
+    public static int abs(int value) {
+        return (value < 0) ? -value : value;
+    }
+
+    public static void main(String[] args) {
+        int value = 5;
+        System.out.println(value + "의 절대값:" + abs(value));
+        value = -10;
+        System.out.println(value + "의 절대값:" + abs(value));
+    }
+}
